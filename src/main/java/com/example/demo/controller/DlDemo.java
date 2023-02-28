b4b1ba8b0ece7c9f024e9d456f5fd3f6fbcefefa
dl <dly13612@163.com>
2023-02-28 22:59:20 +0800

    idea-test-git-master1

--- src/main/java/com/example/demo/controller/DlDemo.java
+++ src/main/java/com/example/demo/controller/DlDemo.java
@@ -8,5 +8,6 @@ public static void main(String[] args) {
      System.out.println("码云新增1");
     System.out.println("idea本地新增1");
     System.out.println("idea本地dev新增1");
+    System.out.println("idea本地master新增1");
 }
 }
